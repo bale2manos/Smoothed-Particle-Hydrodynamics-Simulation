813416fa6c54e2174b43a9fb2342508160b1523d
CarlosMitran <100472086@alumnos.uc3m.es>
2023-11-18 14:25:23 +0100

    progargstest

--- utest/progargs_test.cpp
+++ utest/progargs_test.cpp
@@ -18,10 +18,10 @@ TEST(TestValidateTimeSteps, Test1){
 
 TEST(TestValidateInputFile, Test2){
   std::filesystem::path const currentPath = std::filesystem::current_path();
-  string const pathfile = currentPath.string() + "/";
-  string const testpath = pathfile + "testfiles/small.fld";
-  string nopath = pathfile + "incorrectpath";
-  string corrpath = pathfile + "smallcorr";
+  std::string const pathfile = currentPath.string() + "/";
+  std::string const testpath = pathfile + "testfiles/small.fld";
+  std::string nopath = pathfile + "incorrectpath";
+  std::string corrpath = pathfile + "smallcorr";
   EXPECT_EQ(validate_input_file(testpath.c_str()), 0);
   EXPECT_EQ(validate_input_file(nopath.c_str()), -1);
   EXPECT_EQ(validate_input_file(corrpath.c_str()), -1);
@@ -32,10 +32,10 @@ TEST(TestValidateInputFile, Test2){
 
 TEST(TestValidateOutputFile, Test3){
   std::filesystem::path const currentPath = std::filesystem::current_path();
-  string const pathfile = currentPath.string() + "/";
-  string const testpath = pathfile + "output.fld";
-  string nopath = "";
-  string corrpath = "output.fld";
+  std::string const pathfile = currentPath.string() + "/";
+  std::string const testpath = pathfile + "output.fld";
+  std::string nopath = "";
+  std::string corrpath = "output.fld";
   EXPECT_EQ(validate_output_file(testpath.c_str()), 0);
   EXPECT_EQ(validate_output_file(nopath.c_str()), -1);
   //EXPECT_EQ(validate_output_file(corrpath.c_str()), -1);
@@ -44,10 +44,10 @@ TEST(TestValidateOutputFile, Test3){
 
 TEST(TestValidateParameters, Test4){
   std::filesystem::path const currentPath = std::filesystem::current_path();
-  string const pathfile = currentPath.string() + "/";
-  array <int,2> errortype1,errortype2,errortype3, errortype4;
-  string const file = pathfile + "small.fld";
-  string const file2 = pathfile + "output.fld";
+  std::string const pathfile = currentPath.string() + "/";
+  std::array <int,2> errortype1,errortype2,errortype3, errortype4;
+  std::string const file = pathfile + "small.fld";
+  std::string const file2 = pathfile + "output.fld";
   errortype1 = {-3, 5};
   errortype2 = {-2,-1};
   errortype3 = {-1,0};
@@ -64,41 +64,41 @@ TEST(TestValidateParameters, Test4){
 
 TEST(TestReadInputFile, Test5){
   std::filesystem::path const currentPath = std::filesystem::current_path();
-  string const testpath = currentPath.string() + "/";
-  string const file = testpath + "testfiles/small.fld";
+  std::string const testpath = currentPath.string() + "/";
+  std::string const file = testpath + "testfiles/small.fld";
   Malla const malla = read_input_file(file.c_str());
   Malla const mallaux(4800, 204);
-  EXPECT_EQ(malla.getNp() , 4800);
-  int const nblocks = malla.getNBlocks()[0] *  malla.getNBlocks()[1] *  malla.getNBlocks()[2];
+  EXPECT_EQ(malla.getNumberParticles() , 4800);
+  int const nblocks = malla.getNumberBlocks()[0] *  malla.getNumberBlocks()[1] *  malla.getNumberBlocks()[2];
   EXPECT_EQ(nblocks , 4725);
 }
 
 
 TEST(TestWriteOutputFile, Test6){
   std::filesystem::path const currentPath = std::filesystem::current_path();
-  string const testpath = currentPath.string() + "/";
-  string const file = testpath + "testfiles/small.fld";
-  string const outfile = testpath + "testfiles/output.fld";
+  std::string const testpath = currentPath.string() + "/";
+  std::string const file = testpath + "testfiles/small.fld";
+  std::string const outfile = testpath + "testfiles/output.fld";
   Malla malla = read_input_file(file.c_str());
   write_output_file(malla, outfile.c_str());
   Malla const mallaaux  = read_input_file(outfile.c_str());
-  EXPECT_EQ(malla.getNp(), mallaaux.getNp());
-  EXPECT_EQ(malla.getPpm(), mallaaux.getPpm());
+  EXPECT_EQ(malla.getNumberParticles(), mallaaux.getNumberParticles());
+  EXPECT_EQ(malla.getParticlesPerMeter(), mallaaux.getParticlesPerMeter());
 }
 
 
 TEST(TestCheckNP, Test7){
-  EXPECT_NO_THROW(check_np(1));
-  EXPECT_THROW(check_np(0), runtime_error);
-  EXPECT_THROW(check_np(-1), runtime_error);
+  EXPECT_NO_THROW(check_number_particles(1));
+  EXPECT_THROW(check_number_particles(0), std::runtime_error);
+  EXPECT_THROW(check_number_particles(-1), std::runtime_error);
 }
 
 
 TEST(TestCheckMissmatch, Test8){
-  EXPECT_THROW(check_missmatch_particles(0,1), runtime_error);
-  EXPECT_THROW(check_missmatch_particles(1,0), runtime_error);
-  EXPECT_THROW(check_missmatch_particles(0,-1), runtime_error);
-  EXPECT_THROW(check_missmatch_particles(-1,0), runtime_error);
+  EXPECT_THROW(check_missmatch_particles(0,1), std::runtime_error);
+  EXPECT_THROW(check_missmatch_particles(1,0), std::runtime_error);
+  EXPECT_THROW(check_missmatch_particles(0,-1), std::runtime_error);
+  EXPECT_THROW(check_missmatch_particles(-1,0), std::runtime_error);
   EXPECT_NO_THROW(check_missmatch_particles(0,0));
 
 }
