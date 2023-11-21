5da49b7e259cef1b74ddcbb4fd776bdd101304cb
Javier P <100472252@alumnos.uc3m.es>
2023-11-21 10:43:30 +0100

    clang-tidy errors PASA TESTS

--- sim/progargs.hpp
+++ sim/progargs.hpp
@@ -16,7 +16,7 @@ int validate_time_steps(int nts);
 int validate_input_file(char const * inputFileName);
 int validate_output_file(char const * outputFileName);
 Malla read_input_file(char const * in_file);
-void check_number_particles(int np);
+void check_number_particles(int number_particles);
 int write_output_file(Malla & malla, char const * out_file);
 void check_missmatch_particles(int counter, int malla_np);
 void read_particle_field(bool & finished, std::array<int, 2> indexes, std::ifstream & input_file,
