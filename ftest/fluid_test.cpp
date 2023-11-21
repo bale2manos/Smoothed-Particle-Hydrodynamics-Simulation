e8359cd10272b624d52abc51ba01f40f3618ab43
Pablogspe <100472243@alumnos.uc3m.es>
2023-11-21 20:30:36 +0100

    Codigo comentado estilo Doxygen

--- ftest/fluid_test.cpp
+++ ftest/fluid_test.cpp
@@ -8,6 +8,15 @@
 #include <iostream>
 #include <vector>
 
+/**
+ * @brief Test case for checking the output of small.fld after multiple interactions.
+ *
+ * This test reads the small.fld input file, performs a series of interactions on the grid, and
+ * compares the resulting particle vectors with expected outputs for each iteration.
+ *
+ * @param CheckOutSmall Fixture for checking output of small.fld tests.
+ */
+
 TEST(CheckOutSmall, Test1) {
   std::filesystem::path const currentPath = std::filesystem::current_path();
   std::string const pathsmall             = currentPath.string() + "/testfiles/small.fld";
@@ -23,6 +32,15 @@ TEST(CheckOutSmall, Test1) {
   }
 }
 
+/**
+ * @brief Test case for checking the output of large.fld after multiple interactions.
+ *
+ * This test reads the large.fld input file, performs a series of interactions on the grid, and
+ * compares the resulting particle vectors with expected outputs for each iteration.
+ *
+ * @param CheckOutLarge Fixture for checking output of large.fld tests.
+ */
+
 TEST(CheckOutLarge, Test2) {
   std::filesystem::path const currentPath = std::filesystem::current_path();
   std::string const pathlarge             = currentPath.string() + "/testfiles/large.fld";
@@ -36,6 +54,16 @@ TEST(CheckOutLarge, Test2) {
     EXPECT_EQ(comparevectorsout(malla.getParticles(), large.getParticles()), 0);
   }
 }
+
+/**
+ * @brief Test case for checking the output of small.fld after multiple interactions with trz files.
+ *
+ * This test reads the small.fld input file, performs a series of interactions on the grid, and
+ * compares the resulting particle vectors with expected outputs for each iteration.
+ * Additionally, it checks the trz files for consistency.
+ *
+ * @param CheckOutSmallTrz Fixture for checking output of small.fld with trz files tests.
+ */
 
 TEST(CheckOutSmallTrz, Test3) {
   std::filesystem::path const currentPath = std::filesystem::current_path();
@@ -51,6 +79,17 @@ TEST(CheckOutSmallTrz, Test3) {
   }
 }
 
+/**
+ * @brief Test case for checking the output of large.fld after multiple interactions with trz files.
+ *
+ * This test reads the large.fld input file, performs a series of interactions on the grid, and
+ * compares the resulting particle vectors with expected outputs for each iteration.
+ * Additionally, it checks the trz files for consistency.
+ *
+ * @param CheckOutLargeTrz Fixture for checking output of large.fld with trz files tests.
+ */
+
+
 TEST(CheckOutLargeTrz, Test4) {
   std::filesystem::path const currentPath = std::filesystem::current_path();
   std::string const pathlarge             = currentPath.string() + "/testfiles/large.fld";
@@ -65,6 +104,14 @@ TEST(CheckOutLargeTrz, Test4) {
   }
 }
 
+/**
+ * @brief Compare the particle vectors obtained from simulation with the expected output from a trace file.
+ *
+ * @param malla The simulation grid.
+ * @param inputFileName The name of the trace file to compare against.
+ * @return 0 if the particle vectors match, -1 if the sizes are different, -2 if there is a mismatch in particle attributes.
+ */
+
 int compare_iteration(Malla & malla, char const * inputFileName) {
   std::vector<Particle> particles_out1;
   std::vector<Particle> particles_out2;
@@ -73,6 +120,14 @@ int compare_iteration(Malla & malla, char const * inputFileName) {
   comparevectorstrz(particles_out1, particles_out2);
   return 0;
 }
+
+/**
+ * @brief Compare two vectors of particles for consistency, assuming they are sorted by particle ID.
+ *
+ * @param particles_out1 First vector of particles.
+ * @param particles_out2 Second vector of particles.
+ * @return 0 if the vectors match, -1 if the sizes are different, -2 if there is a mismatch in particle attributes.
+ */
 
 int comparevectorstrz(std::vector<Particle> particles_out1, std::vector<Particle> particles_out2) {
   sort(particles_out1.begin(), particles_out1.end(), [](Particle & a, Particle & b) {
@@ -85,6 +140,14 @@ int comparevectorstrz(std::vector<Particle> particles_out1, std::vector<Particle
   if (particles_out1.size() != particles_out2.size()) { return -1; }
   return compare(particles_out1, particles_out2);
 }
+
+/**
+ * @brief Compare two vectors of particles for consistency, assuming they are sorted by particle ID.
+ *
+ * @param particles_out1 First vector of particles.
+ * @param particles_out2 Second vector of particles.
+ * @return 0 if the vectors match, -2 if there is a mismatch in particle attributes.
+ */
 
 int compare(std::vector<Particle> particles_out1, std::vector<Particle> particles_out2) {
   for (int iter = 0; iter < int(particles_out1.size()); iter++) {
@@ -110,6 +173,14 @@ int compare(std::vector<Particle> particles_out1, std::vector<Particle> particle
   return 0;
 }
 
+/**
+ * @brief Compare two vectors of particles for consistency, assuming they are sorted by particle ID, with tolerance for floating-point comparisons.
+ *
+ * @param particles_out1 First vector of particles.
+ * @param particles_out2 Second vector of particles.
+ * @return 0 if the vectors match, -1 if the sizes are different, -2 if there is a mismatch in particle attributes.
+ */
+
 int comparevectorsout(std::vector<Particle> particles_out1, std::vector<Particle> particles_out2) {
   sort(particles_out1.begin(), particles_out1.end(), [](Particle & a, Particle & b) {
     return a.id < b.id;
@@ -121,6 +192,14 @@ int comparevectorsout(std::vector<Particle> particles_out1, std::vector<Particle
 
   return comparetrzvec(particles_out1, particles_out2);
 }
+
+/**
+ * @brief Compare two vectors of particles for consistency with a tolerance for floating-point comparisons.
+ *
+ * @param particles_out1 First vector of particles.
+ * @param particles_out2 Second vector of particles.
+ * @return 0 if the vectors match, -2 if there is a mismatch in particle attributes.
+ */
 
 int comparetrzvec(std::vector<Particle> particles_out1, std::vector<Particle> particles_out2) {
   double const delta = exp10(-3);
@@ -144,6 +223,13 @@ int comparetrzvec(std::vector<Particle> particles_out1, std::vector<Particle> pa
   return 0;
 }
 
+/**
+ * @brief Read particle data from a trace file.
+ *
+ * @param traceFileName The name of the trace file to read.
+ * @return Vector of particles read from the trace file.
+ */
+
 std::vector<Particle> traceread(char const * traceFileName) {
   std::ifstream traceFile(traceFileName, std::ios::binary);
   int numBlocks = 0;
@@ -160,6 +246,14 @@ std::vector<Particle> traceread(char const * traceFileName) {
 
   return particles_out;
 }
+
+/**
+ * @brief Create a particle from data in a trace file.
+ *
+ * @param traceFile The trace file to read from.
+ * @param iter The iteration number.
+ * @return Particle created from the trace file data.
+ */
 
 Particle makeparticle(std::ifstream & traceFile, int iter) {
   Particle particle{};
@@ -181,15 +275,36 @@ Particle makeparticle(std::ifstream & traceFile, int iter) {
   return particle;
 }
 
+/**
+ * @brief Read a double field from a binary file.
+ *
+ * @param input_file The input file stream.
+ * @param field_to_read The double field to read.
+ */
+
 void read_field_from_file_double(std::ifstream & input_file, auto & field_to_read) {
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
   input_file.read(reinterpret_cast<char *>(&field_to_read), sizeof(double));
 }
 
+/**
+ * @brief Read a long field from a binary file.
+ *
+ * @param input_file The input file stream.
+ * @param field_to_read The long field to read.
+ */
+
 void read_field_from_file_long(std::ifstream & input_file, auto & field_to_read) {
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
   input_file.read(reinterpret_cast<char *>(&field_to_read), sizeof(long));
 }
+
+/**
+ * @brief Read an int field from a binary file.
+ *
+ * @param input_file The input file stream.
+ * @param field_to_read The int field to read.
+ */
 
 void read_field_from_file_int(std::ifstream & input_file, auto & field_to_read) {
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
